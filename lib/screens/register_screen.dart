c0d87d4b5668169d94cc68761ad4f36a03bb8343
Kwanpeemaip <puengchai_k@silpakorn.edu>
2024-10-03 21:57:16 +0700

    register: added logo in appbar

--- lib/screens/register_screen.dart
+++ lib/screens/register_screen.dart
@@ -206,172 +206,178 @@ class _RegisterScreenState extends State<RegisterScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        children: [
-          Center(
-            child: Container(
-              width: 340,
-              height: 550,
-              padding: const EdgeInsets.all(25),
-              decoration: BoxDecoration(
-                color: Colors.white,
-                border: Border.all(
-                  color: const Color(0xFFD9D9D9),
-                  width: 1.0,
+      appBar: AppBar(
+        title: Image.asset(
+          'assets/images/LogoBUS_0.png',
+          height: 50,
+        ),
+        backgroundColor: Colors.white,
+        centerTitle: true,
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            const SizedBox(height: 40),
+            Center(
+              child: Container(
+                width: 340,
+                height: 550,
+                padding: const EdgeInsets.all(25),
+                decoration: BoxDecoration(
+                  color: Colors.white,
+                  border: Border.all(
+                    color: const Color(0xFFD9D9D9),
+                    width: 1.0,
+                  ),
+                  borderRadius: BorderRadius.circular(10),
                 ),
-                borderRadius: BorderRadius.circular(10),
-              ),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: [
-                  const SizedBox(height: 5),
-                  const Text(
-                    'Create New',
-                    style: TextStyle(
-                      fontSize: 25,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  const Text(
-                    'Account',
-                    style: TextStyle(
-                      fontSize: 25,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  const SizedBox(height: 5),
-                  RichText(
-                    text: TextSpan(
-                      children: [
-                        const TextSpan(
-                          text: 'Already Registered? ',
-                          style: TextStyle(
-                            fontSize: 14,
-                            color: Color(0xFF5A5A5A),
-                          ),
-                        ),
-                        TextSpan(
-                          text: 'Sign in ',
-                          style: const TextStyle(
-                            fontSize: 16,
-                            color: Colors.black,
-                            decoration: TextDecoration.underline,
-                          ),
-                          recognizer: TapGestureRecognizer()
-                            ..onTap = () {
-                              Navigator.pushNamed(context, '/');
-                            },
-                        ),
-                        TextSpan(
-                          text: 'here.',
-                          style: TextStyle(
-                            fontSize: 14,
-                            color: Color(0xFF5A5A5A),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                  const SizedBox(height: 25),
-                  Expanded(
-                    child: Padding(
-                      padding: const EdgeInsets.all(7),
-                      child: Form(
-                        key: _formKey,
-                        child: Column(
-                          children: [
-                            _buildTextFormField(
-                              controller: _name,
-                              labelText: 'Name',
-                              icon: Icons.person,
-                              borderColor: Color(0xFFD9D9D9),
-                              focusedBorderColor: Color(0xFF2C2C2C),
-                              validator: (value) =>
-                                  validateName(value, 'Name', 1, 50),
-                            ),
-                            const SizedBox(height: 15),
-                            _buildTextFormField(
-                              controller: _email,
-                              labelText: 'Email',
-                              icon: Icons.email,
-                              borderColor: Color(0xFFD9D9D9),
-                              focusedBorderColor: Color(0xFF2C2C2C),
-                              validator: validateEmail,
-                            ),
-                            const SizedBox(height: 15),
-                            _buildTextFormField(
-                              controller: _password,
-                              labelText: 'Password',
-                              icon: Icons.lock,
-                              borderColor: Color(0xFFD9D9D9),
-                              focusedBorderColor: Color(0xFF2C2C2C),
-                              validator: validatePassword,
-                              obscureText: _obscurePassword,
-                              suffixIcon: IconButton(
-                                icon: Icon(
-                                  _obscurePassword
-                                      ? Icons.visibility
-                                      : Icons.visibility_off,
-                                ),
-                                onPressed: () {
-                                  setState(() {
-                                    _obscurePassword = !_obscurePassword;
-                                  });
-                                },
-                              ),
-                            ),
-                            const SizedBox(height: 15),
-                            _buildTextFormField(
-                              controller: _confirmPassword,
-                              labelText: 'Confirm Password',
-                              icon: Icons.lock_outline,
-                              borderColor: Color(0xFFD9D9D9),
-                              focusedBorderColor: Color(0xFF2C2C2C),
-                              validator: validateConfirmPassword,
-                              obscureText: _obscureConfirmPassword,
-                              suffixIcon: IconButton(
-                                icon: Icon(
-                                  _obscureConfirmPassword
-                                      ? Icons.visibility
-                                      : Icons.visibility_off,
-                                ),
-                                onPressed: () {
-                                  setState(() {
-                                    _obscureConfirmPassword =
-                                        !_obscureConfirmPassword;
-                                  });
-                                },
-                              ),
-                            ),
-                            const SizedBox(height: 40),
-                            registering
-                                ? CircularProgressIndicator()
-                                : ElevatedButton(
-                                    onPressed: registration,
-                                    child: const Text(
-                                      'Register',
-                                      style: TextStyle(color: Colors.white),
-                                    ),
-                                    style: ElevatedButton.styleFrom(
-                                      backgroundColor: const Color(0xFF2C2C2C),
-                                      padding: const EdgeInsets.symmetric(
-                                          horizontal: 108, vertical: 15),
-                                      shape: RoundedRectangleBorder(
-                                        borderRadius: BorderRadius.circular(10),
-                                      ),
-                                    ),
-                                  ),
-                          ],
-                        ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    const SizedBox(height: 5),
+                    const Text(
+                      'Create New',
+                      style: TextStyle(
+                        fontSize: 25,
+                        fontWeight: FontWeight.bold,
                       ),
                     ),
-                  ),
-                ],
+                    const Text(
+                      'Account',
+                      style: TextStyle(
+                        fontSize: 25,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const SizedBox(height: 5),
+                    RichText(
+                      text: TextSpan(
+                        children: [
+                          const TextSpan(
+                            text: 'Already Registered? ',
+                            style: TextStyle(
+                              fontSize: 14,
+                              color: Color(0xFF5A5A5A),
+                            ),
+                          ),
+                          TextSpan(
+                            text: 'Sign in ',
+                            style: const TextStyle(
+                              fontSize: 16,
+                              color: Colors.black,
+                              decoration: TextDecoration.underline,
+                            ),
+                            recognizer: TapGestureRecognizer()
+                              ..onTap = () {
+                                Navigator.pushNamed(context, '/');
+                              },
+                          ),
+                          const TextSpan(
+                            text: 'here.',
+                            style: TextStyle(
+                              fontSize: 14,
+                              color: Color(0xFF5A5A5A),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(height: 25),
+                    Form(
+                      key: _formKey,
+                      child: Column(
+                        children: [
+                          _buildTextFormField(
+                            controller: _name,
+                            labelText: 'Name',
+                            icon: Icons.person,
+                            borderColor: Color(0xFFD9D9D9),
+                            focusedBorderColor: Color(0xFF2C2C2C),
+                            validator: (value) =>
+                                validateName(value, 'Name', 1, 50),
+                          ),
+                          const SizedBox(height: 15),
+                          _buildTextFormField(
+                            controller: _email,
+                            labelText: 'Email',
+                            icon: Icons.email,
+                            borderColor: Color(0xFFD9D9D9),
+                            focusedBorderColor: Color(0xFF2C2C2C),
+                            validator: validateEmail,
+                          ),
+                          const SizedBox(height: 15),
+                          _buildTextFormField(
+                            controller: _password,
+                            labelText: 'Password',
+                            icon: Icons.lock,
+                            borderColor: Color(0xFFD9D9D9),
+                            focusedBorderColor: Color(0xFF2C2C2C),
+                            validator: validatePassword,
+                            obscureText: _obscurePassword,
+                            suffixIcon: IconButton(
+                              icon: Icon(
+                                _obscurePassword
+                                    ? Icons.visibility
+                                    : Icons.visibility_off,
+                              ),
+                              onPressed: () {
+                                setState(() {
+                                  _obscurePassword = !_obscurePassword;
+                                });
+                              },
+                            ),
+                          ),
+                          const SizedBox(height: 15),
+                          _buildTextFormField(
+                            controller: _confirmPassword,
+                            labelText: 'Confirm Password',
+                            icon: Icons.lock_outline,
+                            borderColor: Color(0xFFD9D9D9),
+                            focusedBorderColor: Color(0xFF2C2C2C),
+                            validator: validateConfirmPassword,
+                            obscureText: _obscureConfirmPassword,
+                            suffixIcon: IconButton(
+                              icon: Icon(
+                                _obscureConfirmPassword
+                                    ? Icons.visibility
+                                    : Icons.visibility_off,
+                              ),
+                              onPressed: () {
+                                setState(() {
+                                  _obscureConfirmPassword =
+                                      !_obscureConfirmPassword;
+                                });
+                              },
+                            ),
+                          ),
+                          const SizedBox(height: 40),
+                          registering
+                              ? CircularProgressIndicator()
+                              : ElevatedButton(
+                                  onPressed: registration,
+                                  child: const Text(
+                                    'Register',
+                                    style: TextStyle(color: Colors.white),
+                                  ),
+                                  style: ElevatedButton.styleFrom(
+                                    backgroundColor: const Color(0xFF2C2C2C),
+                                    padding: const EdgeInsets.symmetric(
+                                        horizontal: 108, vertical: 15),
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(10),
+                                    ),
+                                  ),
+                                ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
